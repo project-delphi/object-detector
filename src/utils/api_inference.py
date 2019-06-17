90cf3f723e2786677957254372bc01fa42c216b7
Ravi Kalia <ravkalia@gmail.com>
2019-06-17 13:59:41 -0500

    wip(api): Add API pseudo code

--- src/utils/api_inference.py
+++ src/utils/api_inference.py
@@ -2,10 +2,27 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('trained_model')
-parser.add_argument('detect_dir')
+parser.add_argument('classes')
+parser.add_argument('threshold')
+parser.add_argument('inpath')
 parser.add_argument('outpath')
 args = parser.parse_args()
 
 
-# install dependency
+
+# doesn't work, but intention is here
+import inference
+import subprocess
+
+"""
+ install data and model files
+!pip install keras_retinanet
+
+inference.configure_keras()
+args = (args.trained_model, args.classes, args.threshold, args.inpath)
+model_object = inference.model_object(import_from_drive = True, args = args)
+list(map(in, inference.img_inference))
+subprocess.call(F'mv {inpath} {outpath}')
 # run inference
+
+"""
--- src/utils/api_train.py
+++ src/utils/api_train.py
@@ -5,5 +5,27 @@ parser.add_argument('training_data')
 parser.add_argument('hyperparameters')
 args = parser.parse_args()
 
-# install repo and dependencies
+"""
+ install repo and dependencies
+!git clone https://github.com/project-delphi/object-detector.git
+!mv object-detector/src/utils .
+
+import utils.install as install 
+install.install_retinanet()
+DRIVE_ID = '1xzBJWcjtBDCg10kwjpwZ85t_oXD0a7kt'
+mount_drive_flag = True
+import utils.data as data
+data.ingest(mount_drive_flag, DRIVE_ID)
+# ALTERNATIVE OPTION download via shareable link with mount_drive_flag=False
+data.format()
+# MODEL_PATH = os.path.join('snapshots', sorted(os.listdir('snapshots'), reverse=True)[0])
+MODEL_PATH = '/content/keras-retinanet/snapshots/_pretrained_model.h5'
+import utils.train as train
+train.import_model(drive_model_flag=False, dest=MODEL_PATH, src=None)
 # run training
+#!keras_retinanet/bin/train.py --freeze-backbone --random-transform --weights {MODEL_PATH} --batch-size 8 --lr 1e-3  --steps 3 --epochs 1 csv annotations.csv classes.csv    
+dest='/content/gdrive/My Drive/sargassum/'
+src='./snapshots/resnet50_csv_10.h5'
+import utils.train as train
+train.export_model(dest, src)
+"""
